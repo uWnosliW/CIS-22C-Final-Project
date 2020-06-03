6a6966a41092efa795b643a25647483ddfbad9f2
Wilson <wilson.wu.002@gmail.com>
2020-06-02 22:44:53 -0700

    init

---
+++ Final Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by Wilson Wu on 6/2/20.
+//  Copyright © 2020 None. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
