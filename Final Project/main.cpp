0654145f739fefb122e203c58e67b2f33ab7ce28
Wilson <wilson.wu.002@gmail.com>
2020-06-17 15:38:55 -0700

    added pseudocode to main

--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -1,4 +1,71 @@
 //Yi-wei and Hairun
+/*
+ assumptions and execution:
+ 1) all Airlines in the input file are properly formatted in the following manner:
+    Airline name
+    number of destinations followed by all the destinations space separated(spaces in names are replaced with underscores)
+    on time percentage
+    number of anual complaints
+ 2) console input is taken in the following format
+    command
+    airline name or object
+ */
+/*
+ pseudocode:
+ get the input file path and open the file
+ declare BST, hash table, and linked list with template argument Airlines
+ while not end of file
+    dynamically allocate a temporary airline object
+    read all airline fields and instantiate the airline object
+    insert it into all three data structures
+    delete temporary Airline object
+ get first user input
+ while user input is not quit
+    if input is insert
+        read airline object to insert(assuming correct syntax)
+        insert the object into all three data structures
+    if input is delete:
+        read airline name
+        check if the airline is in the hashtable
+        if it is
+            retreive the airline object from the hashtable
+            use that object to delete instances in the BST and linked list
+        else
+            notify user that the airline is not currently in any data structure
+    if input is search:
+        read airline name
+        check if the airline is in the hashtable
+        search BST for the airline(for efficiency calculations)
+        if airline was found
+            print airline name and its score
+        else
+            notify user that the airline is not currently in any data structure
+     if input is print stats:
+         read airline name
+         check if the airline is in the hashtable
+         search BST for the airline(for efficiency calculations)
+         if airline was found
+             print airline name, score, number of destitions, list of destinations, percent of time on time, and number of anual complaints
+         else
+             notify user that the airline is not currently in any data structure
+     if input is display hash table
+         print hash table
+     if input is print in order
+         print bst in order
+     if input is print indented tree
+        print bst indented
+     if input is print efficiency
+        print hash table load factor, longest linked list, and average linked list size
+        print average operations per bst insertion
+        if searches have been made
+            print average operations per bst search
+        else
+            print no searches have been made yet
+    if input is recomend
+        print top three airlines based on score
+    get next input
+ update input file with elements from linked list
+ */
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -8,12 +75,14 @@
 #include "LinkedList.h"
 using namespace std;
 int main(int argc, const char * argv[]) {
-    string fileName = "/Users/Wilson/Documents/Final Project/Final Project/input.txt";
+    string fileName; //= "/Users/Wilson/Documents/Final Project/Final Project/input.txt";
+    cout<<"Please enter the absolute file path of the input file:"<<endl;
+    getline(cin,fileName);
     ifstream airInput(fileName);
     SinglyLinkedList<Airline> linkedlist;
     HashTable<Airline, 20> hashtable;
     BST<Airline> bst;
-    Airline airlines[50];
+    //Airline airlines[50];
     if (!airInput)
     {
         cerr << "Can't find " << fileName << endl;
@@ -60,7 +129,7 @@ int main(int argc, const char * argv[]) {
             //bst.printInOrder();
             linkedlist.appendNode(*temp);
             hashtable.insert(*temp);
-            airlines[size - 1] = *temp;
+            //airlines[size - 1] = *temp;
             delete []destListTemp;
             delete temp;
         }
@@ -139,6 +208,7 @@ int main(int argc, const char * argv[]) {
             std::string name;
             getline(cin,name);
             Airline temp = hashtable.find(Airline(name));
+            bst.search(temp);
             if(!(temp == Airline()))
             {
                 temp.printStats();
@@ -165,6 +235,7 @@ int main(int argc, const char * argv[]) {
             cout<<"Hash table"<<endl;
             cout<<"load factor: "<<hashtable.getLoadFactor()<<endl;
             cout<<"longest linked list:"<<hashtable.getLongestList()<<endl;
+            cout<<"average linked list size:"<<hashtable.getLongestList()<<endl;
             cout<<"BST"<<endl;
             cout<<"Average operations per insertion: "<<bst.getInsertionOperations()*1.0/bst.getInsertionQueries()<<endl;
             if(bst.getSearchQueries()>0)
